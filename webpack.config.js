51073fd32fdd053258cbb074dbb4ed565cfb4f3a
BigupJeff <me@jeffersonreal.uk>
2025-03-08 03:46:31 +0000

    fluid font and migrate to SASS @use

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const path = require( 'path' )
 const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' )
 // @wordpress/scripts config.
 const wordpressConfig = require( '@wordpress/scripts/config/webpack.config' )
@@ -9,11 +8,11 @@ module.exports = {
 	...wordpressConfig,
 	entry: {
 		// 'example/output': './path/to/dir/entrypoint.js',
-		'css/webguyjeff-theme': path.join( __dirname, '/src/css/webguyjeff-theme.scss' ),
-		'css/webguyjeff-theme-admin': path.join( __dirname, '/src/css/webguyjeff-theme-admin.scss' ),
-		'css/webguyjeff-theme-editor': path.join( __dirname, '/src/css/webguyjeff-theme-editor.scss' ),
-		'css/webguyjeff-theme-dev': path.join( __dirname, '/src/css/webguyjeff-theme-dev.scss' ),
-		'js/webguyjeff-theme': path.join( __dirname, '/src/js/webguyjeff-theme' ),
+		'css/webguyjeff-theme': './src/css/webguyjeff-theme.scss',
+		'css/webguyjeff-theme-admin': './src/css/webguyjeff-theme-admin.scss',
+		'css/webguyjeff-theme-editor': './src/css/webguyjeff-theme-editor.scss',
+		'css/webguyjeff-theme-dev': './src/css/webguyjeff-theme-dev.scss',
+		'js/webguyjeff-theme': './src/js/webguyjeff-theme.js',
 	},
 	plugins: [
 		...wordpressConfig.plugins,
